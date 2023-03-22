214f504a95a1f08a30afdaa154f264089674c3c6
Abhilash Bolla <abhilash.gis@gmail.com>
2023-03-23 01:17:40 +0530

    Added elasticsearch 8.6.0 to package.json

--- common/util.js
+++ common/util.js
@@ -57,6 +57,28 @@ async function index_documents(documents) {
 
                 success = true;
                 done += batch.length;
+
+                if (bulkResponse.errors) {
+                    const erroredDocuments = []
+                    // The items array has the same order of the dataset we just indexed.
+                    // The presence of the `error` key indicates that the operation
+                    // that we did for the document has failed.
+                    bulkResponse.items.forEach((action, i) => {
+                      const operation = Object.keys(action)[0]
+                      if (action[operation].error) {
+                        erroredDocuments.push({
+                          // If the status is 429 it means that you can retry the document,
+                          // otherwise it's very likely a mapping error, and you should
+                          // fix the document before to try it again.
+                          status: action[operation].status,
+                          error: action[operation].error,
+                          operation: operations[i * 2],
+                          document: operations[i * 2 + 1]
+                        })
+                      }
+                    })
+                    console.log(erroredDocuments);
+                  }
             } catch (e) {
                 console.log(e);
                 console.log("Retrying in 10 seconds...");
