2c6ed388048d093e78c9f0315f212048bae4c090
urvishp80 <urvishp80@gmail.com>
2023-12-11 13:44:55 -0500

    Fetch Delving Bitcoin and update to ES

---
+++ delvingbitcoin_2_elasticsearch/achieve.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+"""
+This script does a basic archive of Discourse content by way of its API.
+"""
+import argparse
+import urllib.request
+import sys
+import time
+import os
+import json
+import functools
+import datetime
+from dataclasses import dataclass
+from pathlib import Path
+from dateutil.parser import parse
+
+import logging
+
+loglevel = 'DEBUG' if os.environ.get('DEBUG') else 'INFO'
+
+try:
+    # If `rich` is installed, use pretty logging.
+    from rich.logging import RichHandler
+    logging.basicConfig(level=loglevel, datefmt="[%X]", handlers=[RichHandler()])
+except ImportError:
+    logging.basicConfig(level=loglevel)
+
+log = logging.getLogger('archive')
+
+
+parser = argparse.ArgumentParser(
+    'discourse-archive',
+    description='Create a basic content archive from a Discourse installation')
+parser.add_argument(
+    '-u', '--url', help='URL of the Discourse server',
+    default=os.environ.get('DISCOURSE_URL', 'https://delvingbitcoin.org'))
+parser.add_argument(
+    '--debug', action='store_true', default=os.environ.get('DEBUG'))
+parser.add_argument(
+    '-t', '--target-dir', help='Target directory for the archive',
+    default=Path(os.environ.get('TARGET_DIR', './archive')))
+
+
+@functools.cache
+def args():
+    return parser.parse_args()
+
+
+def http_get(path) -> str:
+    log.debug("HTTP GET %s", path)
+    backoff = 3
+
+    while True:
+        try:
+            with urllib.request.urlopen(f"{args().url}{path}") as f:
+                return f.read().decode()
+        except Exception:
+            time.sleep(backoff)
+            backoff *= 2
+
+            if backoff >= 256:
+                log.exception('ratelimit exceeded, or something else wrong?')
+                sys.exit(1)
+
+
+def http_get_json(path) -> dict:
+    try:
+        return json.loads(http_get(path))
+    except json.JSONDecodeError:
+        log.warning("unable to decode JSON response from %r", path)
+        raise
+
+
+class PostSlug:
+    @classmethod
+    def id_from_filename(cls, name: str) -> int:
+        return int(name.split('-', 1)[0])
+
+
+@dataclass(frozen=True)
+class PostTopic:
+    id: int
+    slug: str
+    title: str
+
+
+@dataclass(frozen=True)
+class Post:
+    id: int
+    slug: str
+    raw: dict
+
+    def get_created_at(self) -> datetime.datetime:
+        # return datetime.datetime.fromisoformat(parse(self.raw['created_at']))
+        return parse(self.raw['created_at'])
+
+    def save(self, dir: Path):
+        """Write the raw post to disk."""
+        idstr = str(self.id).zfill(10)
+        filename = f"{idstr}-{self.raw['username']}-{self.raw['topic_slug']}.json"
+        folder_name = self.get_created_at().strftime('%Y-%m-%B')
+        full_path = dir / folder_name / filename
+        full_path.parent.mkdir(parents=True, exist_ok=True)
+        log.info("saving post %s to %s", self.id, full_path)
+        full_path.write_text(json.dumps(self.raw, indent=2))
+
+    def get_topic(self) -> PostTopic:
+        return PostTopic(
+            id=self.raw['topic_id'],
+            slug=self.raw['topic_slug'],
+            title=self.raw['topic_title'],
+        )
+
+    @classmethod
+    def from_json(cls, j: dict) -> 'Post':
+        return cls(
+            id=j['id'],
+            slug=j['topic_slug'],
+            raw=j,
+        )
+
+
+@dataclass(frozen=True)
+class Topic:
+    id: int
+    slug: str
+    raw: dict
+    markdown: str
+
+    def get_created_at(self) -> datetime.datetime:
+        return parse(self.raw['created_at'])
+
+    def save_rendered(self, dir: Path):
+        """Write the rendered (.md) topic to disk."""
+        date = str(self.get_created_at().date())
+        filename = f"{date}-{self.slug}-id{self.id}.md"
+        folder_name = self.get_created_at().strftime('%Y-%m-%B')
+        full_path = dir / folder_name / filename
+        full_path.parent.mkdir(parents=True, exist_ok=True)
+        log.info("saving topic markdown %s to %s", self.id, full_path)
+        markdown = f"# {self.raw['title']}\n\n{self.markdown}"
+        full_path.write_text(markdown)
+
+    def get_topic(self) -> PostTopic:
+        return PostTopic(
+            id=self.raw['topic_id'],
+            slug=self.raw['topic_slug'],
+            title=self.raw['topic_title'],
+        )
+
+    @classmethod
+    def from_json(cls, t: dict, markdown: str) -> 'Topic':
+        return cls(
+            id=t['id'],
+            slug=t['slug'],
+            raw=t,
+            markdown=markdown,
+        )
+
+
+def download_dumps() -> None:
+    """
+    Sync posts back to `metdata[last_sync_date] - 1 day`, and then save the rendered
+    version of all topics associated with those posts.
+    """
+    target_dir = args().target_dir
+    target_dir = Path(target_dir) if not isinstance(target_dir, Path) else target_dir
+
+    (posts_dir := target_dir / 'posts').mkdir(parents=True, exist_ok=True)
+    (topics_dir := target_dir / 'rendered-topics').mkdir(parents=True, exist_ok=True)
+
+    metadata_file = target_dir / '.metadata.json'
+    last_sync_date = None
+    metadata = {}
+
+    if metadata_file.exists():
+        metadata = json.loads(metadata_file.read_text())
+        last_sync_date = parse(metadata['last_sync_date'])
+
+    if last_sync_date:
+        # Resync over the last day to catch any post edits.
+        last_sync_date -= datetime.timedelta(days=1)
+
+    log.info("detected latest synced post date: %s", last_sync_date)
+
+    topics_to_get = {}
+    max_created_at = None
+    last_created_at: datetime.datetime | None = None
+    last_id: int | None = None
+
+    posts = http_get_json('/posts.json')['latest_posts']
+    no_new_posts = False
+
+    while posts:
+        log.info("processing %d posts", len(posts))
+        for json_post in posts:
+            try:
+                post = Post.from_json(json_post)
+            except Exception:
+                log.warning("failed to deserialize post %s", json_post)
+                raise
+            last_created_at = post.get_created_at()
+
+            if last_sync_date is not None:
+                no_new_posts = last_created_at < last_sync_date
+                if no_new_posts:
+                    break
+
+            post.save(posts_dir)
+
+            if not max_created_at:
+                # Set in this way because the first /post.json result returned will be
+                # the latest created_at.
+                max_created_at = post.get_created_at()
+
+            last_id = post.id
+            topic = post.get_topic()
+            topics_to_get[topic.id] = topic
+
+        if no_new_posts or last_id is not None and last_id <= 1:
+            log.info("no new posts, stopping")
+            break
+
+        time.sleep(5)
+        posts = http_get_json(
+            f'/posts.json?before={last_id - 1}')['latest_posts']
+
+        # Discourse implicitly limits the posts query for IDs between `before` and
+        # `before - 50`, so if we don't get any results we have to kind of scan.
+        while not posts and last_id >= 0:
+            # This is probably off-by-one, but doesn't hurt to be safe.
+            last_id -= 49
+            posts = http_get_json(
+                f'/posts.json?before={last_id}')['latest_posts']
+            time.sleep(1)
+
+    if max_created_at is not None:
+        metadata['last_sync_date'] = max_created_at.isoformat()
+        log.info("writing metadata: %s", metadata)
+        metadata_file.write_text(json.dumps(metadata, indent=2))
+
+    time.sleep(3)
+
+    for topic in topics_to_get.values():
+        try:            
+            data = http_get_json(f"/t/{topic.id}.json")
+            body = http_get(f"/raw/{topic.id}")
+            page_num = 2
+
+            if not body:
+                log.warning("could not retrieve topic %d markdown", topic.id)
+                continue
+
+            while more_body := http_get(f"/raw/{topic.id}?page={page_num}"):
+                body += f"\n{more_body}"
+
+            t = Topic.from_json(data, body)
+            t.save_rendered(topics_dir)
+            log.info("saved topic %s (%s)", t.id, t.slug)
+
+            time.sleep(0.3)
+        except:
+            pass
+
+    return no_new_posts
+    
+
+if __name__ == "__main__":
+    _ = download_dumps()
---
+++ delvingbitcoin_2_elasticsearch/delvingbitcoin_2_elasticsearch.py
@@ -0,0 +1,76 @@
+import os
+import json
+from dotenv import load_dotenv
+
+# from loguru import logger
+from elastic import create_index, document_add, document_exist, document_view
+from achieve import download_dumps
+
+dotenv_path = os.path.join(os.path.dirname(__file__),'..','.env')
+load_dotenv(dotenv_path)
+
+# Get environment variables for path and index name
+INDEX = os.getenv("DELVING_INDEX") or "delvingbitcoin"
+ARCHIVE = os.getenv("ARCHIVE") or "archive"
+SUB_ARCHIVE = os.getenv("SUB_ARCHIVE") or "posts"
+
+# Create Index if it doesn't exist
+if create_index(INDEX):
+    print(f"Index: {INDEX}, created successfully.")
+else:
+    print(f"Index: {INDEX}, already exist.")
+
+# Get the directory where the Python script is located
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Specify the path to the folder containing JSON files
+folder_path = os.path.join(script_dir, ARCHIVE, SUB_ARCHIVE)
+
+
+def index_documents(files_path):
+    # Iterate through files in the specified path
+    for root, dirs, files in os.walk(files_path):
+        for file in files:
+            if file.endswith('.json'):
+                file_path = os.path.join(root, file)
+                print(f'Fetching document from {file_path}')
+
+                # Load JSON data from file
+                with open(file_path, 'r', encoding='utf-8') as json_file:
+                    document = json.load(json_file)
+
+                # Select required fields
+                doc = {
+                    'id': f'{document["id"]}_{document["username"]}_{document["topic_slug"]}_{document["post_number"]}',
+                    'authors': document['username'],
+                    'title': document['topic_title'],
+                    'body': document['raw'],
+                    'body_type': 'raw',
+                    'created_at': document['updated_at'],
+                    'domain': "https://delvingbitcoin.org/",
+                    'url': f"https://delvingbitcoin.org/t/{document['topic_slug']}/{document['topic_id']}",
+                }
+
+                if document['post_number'] != 1:
+                    doc['url'] += f'/{document["post_number"]}'
+
+
+
+                # Check if document already exist
+                resp = document_exist(index_name=INDEX, doc_id=doc['id'])
+                if not resp:
+                    # If not, create document
+                    resp = document_add(index_name=INDEX, doc=doc, doc_id=doc['id'])
+                    print(f'Successfully added {doc["title"]} with the id: {doc["id"]}, Info: {resp}.')
+                else:
+                    print(f"Document with id: {doc['id']} already exist.")
+
+
+if __name__ == "__main__":
+    no_new_posts = download_dumps()
+    if no_new_posts == False:
+        print("New Post found to update on ES!")
+        index_documents(folder_path)
+    else:
+        print("No New Post found to update on ES!")
+    print(f'{("-" * 20)}DONE{("-" * 20)}')
