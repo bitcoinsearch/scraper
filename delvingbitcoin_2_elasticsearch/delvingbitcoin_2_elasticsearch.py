1ad242e7e4017101b39901a0a420e1ab60e1d37d
Urviskumar <urvishp80@gmail.com>
2023-12-19 22:48:01 +0530

    Fetch Delving Bitcoin and update to ES (#59)
    * Fetch Delving Bitcoin and update to ES
    * github workflow - run delvingbitcoin scraper daily
    * Delvingbitcoin To Elasticseach:
    * made fields mimic to mailing list, added more fields like body_formatted, indexed_at, thread_url

---
+++ delvingbitcoin_2_elasticsearch/delvingbitcoin_2_elasticsearch.py
@@ -0,0 +1,103 @@
+import os
+import json
+from dotenv import load_dotenv
+import html
+from bs4 import BeautifulSoup
+from loguru import logger as log
+from datetime import datetime
+
+from elastic import create_index, document_add, document_exist, document_view
+from achieve import download_dumps
+
+dotenv_path = os.path.join(os.path.dirname(__file__),'..','.env')
+load_dotenv(dotenv_path)
+
+# Get environment variables for path and index name
+INDEX = os.getenv("INDEX")
+ARCHIVE = os.getenv("ARCHIVE") or "archive"
+SUB_ARCHIVE = os.getenv("SUB_ARCHIVE") or "posts"
+
+# Create Index if it doesn't exist
+if create_index(INDEX):
+    log.info(f"Index: {INDEX}, created successfully.")
+else:
+    log.info(f"Index: {INDEX}, already exist.")
+
+# Specify the path to the folder containing JSON files
+folder_path = os.path.join(os.getcwd(), ARCHIVE, SUB_ARCHIVE)
+
+
+def preprocess_body(text):
+    soup = BeautifulSoup(text, "html.parser")
+    text = soup.get_text()
+    text = html.unescape(text)
+    text = text.strip()
+    return text
+
+def strip_attributes(html):
+    soup = BeautifulSoup(html, "html.parser")
+    for tag in soup.find_all():
+        tag.attrs = {}
+    return str(soup)
+
+
+def strip_attributes_but_urls(html):
+    soup = BeautifulSoup(html, "html.parser")
+    for tag in soup.find_all():
+        if tag.name not in ['a', 'img']:  # preserve a and img tags
+            tag.attrs = {}
+        else:  # for a and img tags, preserve href and src respectively
+            attrs = dict(tag.attrs)
+            for attr in attrs:
+                if attr not in ['href', 'src']:
+                    del tag.attrs[attr]
+    return str(soup)
+
+
+def index_documents(files_path):
+    # Iterate through files in the specified path
+    for root, dirs, files in os.walk(files_path):
+        for file in files:
+            if file.endswith('.json'):
+                file_path = os.path.join(root, file)
+                log.info(f'Fetching document from file: {file_path}')
+
+                # Load JSON data from file
+                with open(file_path, 'r', encoding='utf-8') as json_file:
+                    document = json.load(json_file)
+
+                # Select required fields
+                doc = {
+                    'id': f'{document["id"]}_{document["username"]}_{document["topic_slug"]}_{document["post_number"]}',
+                    'authors': [document['username']],
+                    'thread_url': f"https://delvingbitcoin.org/t/{document['topic_slug']}/{document['topic_id']}",
+                    'title': document['topic_title'],
+                    'body_type': 'html',
+                    'body': preprocess_body(document['raw']),
+                    'body_formatted': strip_attributes_but_urls(document['cooked']),
+                    'created_at': document['updated_at'],
+                    'domain': "https://delvingbitcoin.org/",
+                    'url': f"https://delvingbitcoin.org/t/{document['topic_slug']}/{document['topic_id']}",
+                    "indexed_at": datetime.utcnow().isoformat()
+                }
+
+                if document['post_number'] != 1:
+                    doc['url'] += f'/{document["post_number"]}'
+                    doc['type'] = 'reply'
+                else:
+                    doc['type'] = 'original_post'
+
+                # Check if document already exist
+                resp = document_view(index_name=INDEX, doc_id=doc['id'])
+                if not resp:
+                    resp = document_add(index_name=INDEX, doc=doc, doc_id=doc['id'])
+                    log.success(f'Successfully added! ID: {doc["id"]}, Type:{doc["type"]}')
+                else:
+                    log.info(f"Document already exist! ID: {doc['id']}")
+
+
+if __name__ == "__main__":
+    no_new_posts = download_dumps()
+    log.info(f"Looking data in folder path: {folder_path}")
+    index_documents(folder_path)
+    log.info(f'{("-" * 20)}DONE{("-" * 20)}')
---
+++ delvingbitcoin_2_elasticsearch/elastic.py
@@ -0,0 +1,75 @@
+import os
+from dotenv import load_dotenv
+from elasticsearch import Elasticsearch, NotFoundError, BadRequestError
+from loguru import logger as log
+load_dotenv()
+
+es = Elasticsearch(
+        cloud_id=os.getenv("CLOUD_ID"),
+        api_key=os.getenv("USER_PASSWORD")
+    )
+
+
+def create_index(index_name):
+    """Functionality to create index."""
+    try:
+        resp = es.indices.create(index=index_name)
+    except BadRequestError:
+        resp = False
+    return resp
+
+
+def document_add(index_name, doc, doc_id=None):
+    """Funtion to add a document by providing index_name,
+    document type, document contents as doc and document id."""
+    resp = es.index(index=index_name, body=doc, id=doc_id)
+    return resp
+
+
+def document_view(index_name, doc_id):
+    """Function to view document."""
+    try:
+        resp = es.get(index=index_name, id=doc_id)
+    except NotFoundError:
+        resp = False
+    return resp
+
+
+def document_update(index_name, doc_id, doc=None, new=None):
+    """Function to edit a document either updating existing fields or adding a new field."""
+    if doc:
+        resp = es.index(index=index_name, id=doc_id, body=doc)
+    else:
+        resp = es.update(index=index_name, id=doc_id, body={"doc": new})
+    return resp
+
+
+def document_delete(index_name, doc_id):
+    """Function to delete a specific document."""
+    resp = es.delete(index=index_name, id=doc_id)
+    return resp
+
+
+def delete_index(index_name):
+    """Delete an index by specifying the index name"""
+    resp = es.indices.delete(index=index_name)
+    return resp
+
+def document_exist(index_name, doc_id):
+    body = {
+        "query" : {
+            "bool":{
+                "must":[
+                    {
+                        "term" : {
+                            "id.keyword" :  doc_id
+                        }
+                    }
+                ]
+            }
+        }
+    }
+
+    resp = es.count(index=index_name, body=body)
+
+    return resp["count"] > 0 
