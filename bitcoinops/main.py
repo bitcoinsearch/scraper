d09a50d88ea4945d17dd4bcac7a55771d5bc5e62
urvishp80 <urvishp80@gmail.com>
2024-06-07 09:05:33 -0400

    Bitcoin Optech: - resolve: Empty document body #68

--- bitcoinops/main.py
+++ bitcoinops/main.py
@@ -1,55 +1,67 @@
-import argparse
+import asyncio
+import os
 import re
+import zipfile
 from datetime import datetime
-from os import walk, getenv, makedirs
-from os.path import join, exists, basename
 
-import dotenv
 import requests
 import yaml
-import zipfile
+from dotenv import load_dotenv
 from loguru import logger
 
 from common.elasticsearch_utils import upsert_document
 
+load_dotenv()
 
-def download_zip(source_url: str, download_dir, file_name="raw_data.zip", extract=True):
+FOLDER_NAME = "raw_data"
+REPO_URL = "https://github.com/bitcoinops/bitcoinops.github.io/archive/refs/heads/master.zip"
+POST_DIR = "bitcoinops.github.io-master/_posts/en"
+TOPIC_DIR = "bitcoinops.github.io-master/_topics/en"
+
+INDEX_NAME = os.getenv('INDEX')
+DATA_DIR = os.getenv('DATA_DIR')
+
+# Paths
+DIR_PATH = os.path.join(DATA_DIR, "bitcoinops_dir")
+GLOBAL_URL_VARIABLE = os.path.join(DIR_PATH, FOLDER_NAME)
+
+
+async def download_repo():
+    os.makedirs(DIR_PATH, exist_ok=True)
+
+    if os.path.exists(GLOBAL_URL_VARIABLE):
+        logger.info(f"Repo already downloaded at path: {DIR_PATH}")
+        return
+
+    logger.info(f"Downloading repo at path: {DIR_PATH}")
+    file_path = os.path.join(DIR_PATH, "raw_data.zip")
+
     try:
-        if not exists(join(download_dir, file_name)):
-            makedirs(download_dir, exist_ok=True)
-            logger.info("Downloading Repo from {}.".format(source_url))
-            response = requests.get(source_url)
-            with open(join(download_dir, file_name), "wb") as zip_file:
-                zip_file.write(response.content)
-            print("Download Complete, repo stored at {}".format(join(download_dir, file_name)))
-            if extract:
-                logger.info("Extracting repo at {}.".format(join(download_dir, file_name[:-4:])))
-                zip_file = zipfile.ZipFile(join(download_dir, file_name))
-                zip_file.extractall(join(download_dir, file_name[:-4:]))
-        else:
-            logger.warning("Repo already downloaded")
-        return True
-    except Exception as e:
-        logger.error(e)
-        return False
+        response = requests.get(REPO_URL)
+        response.raise_for_status()
+
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+        logger.info(f"Downloaded {REPO_URL} to {file_path}")
+
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall(DIR_PATH)
+        logger.info(f"Unzipped {file_path} to {DIR_PATH}")
+
+    except requests.RequestException as e:
+        logger.error(f"Failed to download the repo: {e}")
+
+    except zipfile.BadZipFile as e:
+        logger.error(f"Failed to unzip the file: {e}")
 
 
-def dir_walk(extracted_dir: str, typeof: str):
-    if exists(extracted_dir):
-        documents = []
-        for file in walk(extracted_dir):
-            dirs = file[1]
-            files = file[2]
-            for dir in dirs:
-                documents.extend(dir_walk(join(file[0], dir), typeof))
-                continue
-            for post_file in files:
-                logger.info("Parsing {}".format(join(file[0], post_file)))
-                documents.append(parse_post(join(file[0], post_file), typeof))
-            return documents
-
-    else:
-        logger.critical("Data Directory not available.")
+def parse_markdowns(content: str):
+    sections = re.split(r'---\n', content)
+    if len(sections) < 3:
+        raise ValueError("Input text does not contain proper front matter delimiters '---'")
+    front_matter = sections[1].strip()
+    body = sections[2].strip()
+    return front_matter, body
 
 
 def parse_post(post_file: str, typeof: str):
@@ -59,87 +71,60 @@ def parse_post(post_file: str, typeof: str):
         content = re.sub(r'{%.*%}', '', content, flags=re.MULTILINE)
         front_matter, body = parse_markdowns(content)
         metadata = yaml.safe_load(front_matter)
-        custom_id = basename(post_file).replace('.md', '') if typeof == 'topic' else metadata['slug']
+        custom_id = os.path.basename(post_file).replace('.md', '') if typeof == 'topic' else metadata['slug']
         document = {
-            "id": "bitcoinops-" + custom_id,
+            "id": f"bitcoinops-{custom_id}",
             "title": metadata['title'],
             "body_formatted": body,
             "body": body,
             "body_type": "markdown",
-            "created_at": metadata['date'].strftime('%Y-%m-%dT%H:%M:%S.000Z') if metadata.get('date') else None,
+            "created_at": metadata.get('date').strftime('%Y-%m-%dT%H:%M:%S.000Z') if metadata.get('date') else None,
             "domain": "https://bitcoinops.org/en/",
-            "url": "https://bitcoinops.org/en/topics/{}".format(
-                custom_id + '.md') if typeof == "topic" else "https://bitcoinops.org${}".format(metadata['permalink']),
+            "url": f"https://bitcoinops.org/en/topics/{custom_id}" if typeof == "topic" else f"https://bitcoinops.org${metadata['permalink']}",
             "type": "topic" if typeof == "topic" else metadata['type'],
-            "language": metadata['lang'] if 'lang' in metadata.keys() else 'en',
+            "language": metadata.get('lang', 'en'),
             "authors": ["bitcoinops"],
             "indexed_at": datetime.now().isoformat()
         }
         return document
     except IOError as e:
-        logger.warning("Issue while parsing the file, {}".format(post_file))
+        logger.warning(f"Issue while parsing the file, {post_file}: {e}")
+        return None
 
 
-def parse_markdowns(content: str):
-    lines = content.split("\n")
-    front_matter = False
-    body_flag = False
-    front_matter_list = []
-    body_list = []
-
-    for line in lines:
-        if line.startswith("---"):
-            if front_matter:
-                front_matter = False
-                body_flag = True
-                continue
-            if body_flag:
-                break
-            front_matter = True
-            continue
-        if front_matter:
-            front_matter_list.append(line)
-        elif body_flag:
-            body_list.append(line)
-    return '\n'.join(front_matter_list), '\n'.join(body_list)
-
-
-def parseTopics():
-    pass
-
-
-def main(data_source_url: str, download_dir: str, file_name: str, postdir_path: str, topicdir_path: str) -> None:
-    if download_zip(data_source_url, download_dir, file_name):
-        all_post = dir_walk(join(download_dir, file_name[:-4:], postdir_path), "posts")
-        all_topic = dir_walk(join(download_dir, file_name[:-4:], topicdir_path), "topic")
-        all_post.extend(all_topic)
-        cnt = 0
-        for post in all_post:
-            try:
-                res = upsert_document(index_name=getenv('INDEX'), doc_id=post['id'], doc_body=post)
-                cnt += 1
-                logger.info("Version-{}, Result-{}, ID-{}".format(res['_version'], res['result'], res['_id']))
-            except Exception as e:
-                logger.error("Error: {}".format(e))
-                logger.warning(post)
-        logger.info("Total Post Inserted/Updated-{}".format(cnt))
+def dir_walk(extracted_dir: str, typeof: str):
+    if os.path.exists(extracted_dir):
+        documents = []
+        for root, dirs, files in os.walk(extracted_dir):
+            for dir in dirs:
+                documents.extend(dir_walk(os.path.join(root, dir), typeof))
+            for post_file in files:
+                logger.info(f"Parsing {os.path.join(root, post_file)}")
+                document = parse_post(os.path.join(root, post_file), typeof)
+                if document:
+                    documents.append(document)
+        return documents
     else:
-        raise Exception
+        logger.critical("Data Directory not available.")
+        return []
 
 
-if __name__ == "__main__":
-    dotenv.load_dotenv()
-    parser = argparse.ArgumentParser(description="Bitcoin Optech scraper")
-    parser.add_argument("-U", "--sourceUrl",
-                        default="https://github.com/bitcoinops/bitcoinops.github.io/archive/refs/heads/master.zip")
-    parser.add_argument("-D", "--downloadDir", default=getenv("DATA_DIR"))
-    parser.add_argument("-F", "--fileName", default="raw_data.zip")
-    parser.add_argument("-P", "--postDir", default="bitcoinops.github.io-master/_posts/en")
-    parser.add_argument("-T", "--topicDir", default="bitcoinops.github.io-master/_topics/en")
-    args = parser.parse_args()
-    url = args.sourceUrl
-    download_dir = args.downloadDir
-    output_file_name = args.fileName
-    post_dir = args.postDir
-    topic_dir = args.topicDir
-    main(url, download_dir, output_file_name, post_dir, topic_dir)
+async def main():
+    await download_repo()
+    all_posts = dir_walk(os.path.join(DIR_PATH, POST_DIR), "posts")
+    all_topics = dir_walk(os.path.join(DIR_PATH, TOPIC_DIR), "topic")
+    cnt = 0
+    all_posts.extend(all_topics)
+    for post in all_posts:
+        try:
+            res = upsert_document(index_name=INDEX_NAME, doc_id=post['id'], doc_body=post)
+            cnt += 1
+            logger.info(f"Version-{res['_version']}, Result-{res['result']}, ID-{res['_id']}")
+        except Exception as e:
+            logger.error(f"Error: {e}")
+            logger.warning(post)
+    logger.info(f"Total Post Inserted/Updated-{cnt}")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
