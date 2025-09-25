c680be8f75baf57852ab795757f9247c534afe7d
jrakibi <j.errakibi@gmail.com>
2025-09-25 17:38:28 +0100

    fix env variable

--- config/conf.py
+++ config/conf.py
@@ -14,18 +14,8 @@ START_INDEX = os.getenv("START_INDEX", 0)
 
 ES: Elasticsearch
 
-# Get ES_URL from environment for URL-based connections
-ES_URL = os.getenv("ES_URL", None)
-
 if ES_LOCAL_URL is not None:
     ES = Elasticsearch(ES_LOCAL_URL)
-elif ES_URL is not None and API_KEY is not None:
-    # Use URL-based connection with API key in headers format
-    ES = Elasticsearch(
-        ES_URL,
-        headers={'Authorization': f'ApiKey {API_KEY}'},
-        request_timeout=30
-    )
 elif API_KEY is not None and CLOUD_ID is not None:
     ES = Elasticsearch(
         cloud_id=CLOUD_ID,
